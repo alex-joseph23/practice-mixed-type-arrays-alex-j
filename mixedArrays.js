98513d581627ee011e586fb043b86b98d17f3dc9
Alex Joseph <alexandra.k.joseph2323@gmail.com>
2025-03-29 19:14:13 -0500

    Profile overview

---
+++ mixedArrays.js
@@ -0,0 +1,6 @@
+let userProfile = ["Kristen Keele", 30, true, { city: "Wayne" }, ["dancing", "pilates"]];
+console.log("Username:", userProfile[0]);
+console.log("Hobbies Include:", userProfile[4][1]);
+userProfile[1] = 31;
+userProfile[4] = ["dancing", "pilates", "reading"]
+console.log(userProfile);
