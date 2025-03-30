420e78494335eb4ac4734f973cf99ea23267aaa9
alex-joseph23 <alexandra.k.joseph2323@gmail.com>
2025-03-29 23:11:34 -0500

    mixedArrays.js

--- mixedArrays.js
+++ mixedArrays.js
@@ -1,4 +1,4 @@
-let userProfile = ["Kristen Keele", 30, true, { city: "Wayne" }, ["dancing", "pilates"]];
+let userProfile = ["Bridget Medler", 30, true, { city: "Wayne" }, ["dancing", "pilates"]];
 console.log("Username:", userProfile[0]);
 console.log("Hobbies Include:", userProfile[4][1]);
 userProfile[1] = 31;
